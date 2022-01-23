42a37cb27baa554fbe2ce50967de644321e69c8f
vu2rmk <vu2rmk@gmail.com>
2022-01-23 21:56:20 +0530

    add student functionality

--- src/main/java/com/vu2rmk/studentmanagementsystem/StudentManagementSystemApplication.java
+++ src/main/java/com/vu2rmk/studentmanagementsystem/StudentManagementSystemApplication.java
@@ -1,13 +1,31 @@
 package com.vu2rmk.studentmanagementsystem;
 
+import com.vu2rmk.studentmanagementsystem.entity.Student;
+import com.vu2rmk.studentmanagementsystem.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class StudentManagementSystemApplication {
+public class StudentManagementSystemApplication implements CommandLineRunner {
+
+	@Autowired
+	private StudentRepository studentRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(StudentManagementSystemApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+//		Student student1 = new Student("Ram","Krishna", "vu2rmk@gmail.com");
+//		studentRepository.save(student1);
+//
+//		Student student = new Student("Sanjay", "Jadhav", "sanjay@gmail.com");
+//		studentRepository.save(student);
+//
+//		Student student2 = new Student("Rah", "Ver", "rah@gmail.com");
+//		studentRepository.save(student2);
+	}
 }
--- src/main/java/com/vu2rmk/studentmanagementsystem/controller/StudentController.java
+++ src/main/java/com/vu2rmk/studentmanagementsystem/controller/StudentController.java
@@ -1,9 +1,12 @@
 package com.vu2rmk.studentmanagementsystem.controller;
 
+import com.vu2rmk.studentmanagementsystem.entity.Student;
 import com.vu2rmk.studentmanagementsystem.service.StudentService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class StudentController {
@@ -19,5 +22,18 @@ public class StudentController {
     public String listStudents(Model model){
         model.addAttribute("students",studentService.getAllStudents());
         return "students";
+    }
+
+    @GetMapping("/students/new")
+    public String createStudentForm(Model model){
+        Student student = new Student();
+        model.addAttribute("student", student);
+        return "create_student";
+    }
+
+    @PostMapping("/students")
+    public String saveStudent(@ModelAttribute("student") Student student){
+        studentService.saveStudent(student);
+        return "redirect:/students";
     }
 }
